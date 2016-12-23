d7cf9e8a9421418c19001e493418d1c3a5d8d6c1
Jay Little <jaylittle@jaylittle.com>
2016-12-23 17:41:59 -0500

    Initial Commit

---
+++ pi-top-manager-client.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import os
+from spidev import SpiDev
+import subprocess
+import time
+
+# TODO: Rewrite this code
+def setupSPI():
+    spi = SpiDev()
+    spi.open(0,1) # Bus 0, Chip Select 1
+	spi.max_speed_hz = 9600
+	spi.mode = 0b00
+	spi.bits_per_word = 8
+	spi.cshigh = True
+	spi.lsbfirst = False
+	return spi
+
+
+
