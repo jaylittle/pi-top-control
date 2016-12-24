d116b07de8892e3b2b88b89ba872d4efed04540f
Jay Little <jaylittle@jaylittle.com>
2016-12-24 09:45:13 -0500

    Added functional version of system state read

--- pi-top-manager-client.py
+++ pi-top-manager-client.py
@@ -2,6 +2,7 @@
 
 import sys
 import smbus
+from spidev import SpiDev
 
 # Pi-Top Battery Address & Registers
 PI_BUS = 1
@@ -13,15 +14,96 @@ PI_TOP_BATTERY_REGISTER_CHARGING_TIME = 0x13
 PI_TOP_BATTERY_REGISTER_CAPACITY = 0x0d
 PI_TOP_BATTERY_STATE_MASK = int('1000000000000000', 2)
 
+PI_TOP_HUB_COMMAND_GET_STATUS = 0xFF
+
+PI_TOP_BRIGHTNESS_PARITY_MASK = 0x80
+PI_TOP_BRIGHTNESS_MASK = 0x78
+PI_TOP_LID_OPEN_MASK = 0x04
+PI_TOP_STATE_PARITY_MASK = 0x04
+PI_TOP_SCREEN_OFF_MASK = 0x02
+PI_TOP_POWER_OFF_MASK = 0X01
+
 # Valid Devices and Command lists
-ARG_DEVICE_LIST = ["battery"]
+ARG_DEVICE_LIST = ["battery", "system", "backlight"]
 ARG_COMMAND_LIST = {
-    "battery": ["state","capacity","time"]
+    "battery": ["state","capacity","time"],
+    "system": ["state"],
+    "backlight": ["increase", "decrease", "on", "off"]
 }
 
-bus = smbus.SMBus(PI_BUS)
+class RequestResult:
+    data = None
+    formattedData = None
+    errorMessage = None
+    def __init__(self, data = None, formatted_data = None, error_message = None):
+        self.data = data
+        self.formattedData = formatted_data
+        self.errorMessage = error_message
+
+class HubState:
+    power_off = 0
+    screen_off = 0
+    state_parity = 0
+    brightness = 5
+    brightness_parity = 0
+    lid_open = 0
+
+    def decode(self, data):
+        self.power_off = data & PI_TOP_POWER_OFF_MASK
+        self.screen_off = (data & PI_TOP_SCREEN_OFF_MASK) >> 1
+        self.lid_open = (data & PI_TOP_LID_OPEN_MASK) >> 2
+        self.state_parity = self.parityOf(data & 3)
+        self.brightness = (data & PI_TOP_BRIGHTNESS_MASK) >> 3
+        self.brightness_parity = self.parityOf(self.brightness)
+
+    def encode(self):
+        result = self.brightness << 3
+        if self.parityOf(result & PI_TOP_BRIGHTNESS_MASK):
+            result = result + PI_TOP_BRIGHTNESS_PARITY_MASK
+        if self.power_off:
+            result = result + PI_TOP_POWER_OFF_MASK
+        if self.screen_off:
+            result = result + PI_TOP_SCREEN_OFF_MASK
+        if self.parityOf(result & 3):
+            result = result + PI_TOP_STATE_PARITY_MASK
+        return result
+
+    def parityOf(self, data):
+        parity = 0
+        for bit in bin(data)[2:]:
+            parity ^= int(bit)
+        return parity
+
+    def __str__(self):
+        result = "{ \"power off\": %s" % self.power_off
+        result += ", \"screen_off\": %s" % self.screen_off
+        result += ", \"state_parity\": %s" % self.state_parity
+        result += ", \"lid_open\": %s" % self.lid_open
+        result += ", \"brightness\": %s" % self.brightness
+        result += ", \"brightness_parity\": %s }" % self.brightness_parity
+        return result
+
+bus = None
+spi = None
+
+def configBus():
+    global bus
+    if bus == None:
+        bus = smbus.SMBus(PI_BUS)
+
+def configSpi():
+    global spi
+    if spi == None:
+        spi = SpiDev()
+        spi.open(0, 1)  # Bus 0, Chip Select 1
+        spi.max_speed_hz = 9600
+        spi.mode = 0b00
+        spi.bits_per_word = 8
+        spi.cshigh = True
+        spi.lsbfirst = False
 
 def busGetData(address, register, length = 2):
+    configBus()
     readAttempt = 1
     success = False
     data = None
@@ -37,10 +119,27 @@ def busGetData(address, register, length = 2):
 
     return data
 
-def batteryGetState(printFlag = True):
-    result = "Unknown"
+def busWriteData(address, register, data):
+    # TODO Figure this out
+    configBus()
+
+def spiWriteData(data):
+    configSpi()
+    spi.cshigh = False
+    data = spi.xfer(data, spi.max_speed_hz)
+    spi.cshigh = True
+
+    for chunk in data:
+        chunk_hex_str = '0x' + str(hex(chunk))[2:].zfill(2)
+        chunk_bin_str = '{0:b}'.format(int(chunk_hex_str[2:], 16)).zfill(8)
+        print("spi write returned %s" % chunk_bin_str)
+
+    return data
+
+def batteryGetState():
+    result = RequestResult("Unknown")
     readAttempt = 1
-    while result == "Unknown" and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
+    while result.data == "Unknown" and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
         rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_STATE)
 
         # TODO Research zen buddhism and acquire a better understanding of the logic below
@@ -51,63 +150,78 @@ def batteryGetState(printFlag = True):
 
         if (data >= -4000) and (data <= 4000):
             if data >= -10:
-                result = "Charging"
+                result.data = "Charging"
             else:
-                result = "Discharging"
+                result.data = "Discharging"
 
-        readAttempt = readAttempt + 1
+        readAttempt += 1
 
     if readAttempt > PI_TOP_MAX_READ_ATTEMPTS:
-        print("Bus read failed %i times in a row." % readAttempt, file=sys.stderr)
-        exit(1)
-
-    if printFlag:
-        print("%s" % result)
+        result.error = "Bus read failed %i times in a row." % readAttempt
+    else:
+        result.formattedData = result.data
 
     return result
 
-def batteryGetCapacity(printFlag = True):
-    result = None
+def batteryGetCapacity():
+    result = RequestResult()
     readAttempt = 1
-    while result == None and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
+    while result.data == None and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
         rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_CAPACITY)
         if rawData <= 100:
-            result = rawData
+            result.data = rawData
 
-        readAttempt = readAttempt + 1
+        readAttempt += 1
 
     if readAttempt > PI_TOP_MAX_READ_ATTEMPTS:
-        print("Bus read failed %i times in a row." % readAttempt, file=sys.stderr)
-        exit(1)
-
-    if printFlag:
-        print("%s%%" % result)
+        result.error = "Bus read failed %i times in a row." % readAttempt
+    else:
+        result.formattedData = "%s%%" % result.data
 
     return result
 
-def batteryGetTime(printFlag = True):
-    batteryState = batteryGetState(False)
+def batteryGetTime():
+    batteryState = batteryGetState()
 
-    result = None
+    if batteryState.errorMessage == None:
+        result = RequestResult()
+        readAttempt = 1
+        while result.data == None and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
+            if (batteryState.data == "Charging"):
+                rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_CHARGING_TIME)
+                if (rawData <= 2400):
+                    result.data = rawData
+            elif (batteryState.data == "Discharging"):
+                rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_DISCHARGE_TIME)
+                if (rawData <= 1800):
+                    result.data = rawData
+
+            readAttempt += 1
+
+        if readAttempt > PI_TOP_MAX_READ_ATTEMPTS:
+            result.errorMessage = "Bus read failed %i times in a row." % readAttempt
+        else:
+            result.formattedData = "%i mins" % result.data
+
+        return result
+
+    return batteryState
+
+def systemGetState():
+    result = RequestResult()
     readAttempt = 1
-    while result == None and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
-        if (batteryState == "Charging"):
-            rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_CHARGING_TIME)
-            if (rawData <= 2400):
-                result = rawData
-        elif (batteryState == "Discharging"):
-            rawData = busGetData(PI_TOP_BATTERY_ADDRESS, PI_TOP_BATTERY_REGISTER_DISCHARGE_TIME)
-            if (rawData <= 1800):
-                result = rawData
-
-        readAttempt = readAttempt + 1
+    while result.data == None and readAttempt <= PI_TOP_MAX_READ_ATTEMPTS:
+        rawData = spiWriteData([PI_TOP_HUB_COMMAND_GET_STATUS])
+        if len(rawData) >= 1:
+            state = HubState()
+            state.decode(rawData[0])
+            result.data = state
+            readAttempt += 1
 
     if readAttempt > PI_TOP_MAX_READ_ATTEMPTS:
-        print("Bus read failed %i times in a row." % readAttempt, file=sys.stderr)
-        exit(1)
-
-    if printFlag:
-        print("%i mins" % result)
+        result.errorMessage = "Bus read failed %i times in a row." % readAttempt
+    else:
+        result.formattedData = "%s" % result.data
 
     return result
 
@@ -119,7 +233,7 @@ if len(sys.argv) <= 2:
 device = sys.argv[1].lower()
 command = sys.argv[2].lower()
 
-if device not in ["battery"]:
+if device not in ARG_DEVICE_LIST:
     print("Error: Device must be one of the following: %s" % ARG_DEVICE_LIST, file=sys.stderr)
     quit(1)
 
@@ -127,10 +241,20 @@ if command not in ARG_COMMAND_LIST.get(device):
     print("Error: Command must be one of the following: %s" % ARG_COMMAND_LIST.get(device), file=sys.stderr)
     quit(1)
 
+result = RequestResult()
+
 if device == "battery":
     if command == "state":
-        batteryGetState()
+        result = batteryGetState()
     if command == "capacity":
-        batteryGetCapacity()
+        result = batteryGetCapacity()
     if command == "time":
-        batteryGetTime()
+        result = batteryGetTime()
+elif device == "system":
+    if command == "state":
+        result = systemGetState()
+
+if result.errorMessage != None:
+    print("Error: %s" % result.errorMessage, file=sys.stderr)
+else:
+    print(result.formattedData)
--- pi-top-manager-daemon.py
+++ pi-top-manager-daemon.py
@@ -1,17 +1,4 @@
-from datetime import datetime
-import os
+import sys
 from spidev import SpiDev
-import subprocess
-import time
-
-# TODO: Rewrite this code
-def setupSPI():
-    spi = SpiDev()
-    spi.open(0,1) # Bus 0, Chip Select 1
-	spi.max_speed_hz = 9600
-	spi.mode = 0b00
-	spi.bits_per_word = 8
-	spi.cshigh = True
-	spi.lsbfirst = False
-	return spi
+import smbus
 
